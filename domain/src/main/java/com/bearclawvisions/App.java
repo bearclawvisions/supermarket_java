3e09003176d7b982ddda0847d886478d0a581740
bearclawvisions <113764031+bearclawvisions@users.noreply.github.com>
2025-12-23 18:29:38 +0100

    add other libs

---
+++ domain/src/main/java/com/bearclawvisions/App.java
@@ -0,0 +1,13 @@
+package com.bearclawvisions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
