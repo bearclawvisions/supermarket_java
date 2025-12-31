5f967e45f0aadb761deb076cbda5b77056439653
bearclawvisions <113764031+bearclawvisions@users.noreply.github.com>
2025-12-31 13:47:43 +0100

    no api annotations for controller

--- api/src/main/java/com/bearclawvisions/api/controllers/AuthController.java
+++ api/src/main/java/com/bearclawvisions/api/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.bearclawvisions.api.controllers;
 
-import com.bearclawvisions.api.annotations.ApiController;
 import com.bearclawvisions.api.contracts.ApiResponse;
 import com.bearclawvisions.api.helpers.ApiResponseBuilder;
 import com.bearclawvisions.api.helpers.JwtUtil;
@@ -12,10 +11,13 @@ import com.bearclawvisions.services.interfaces.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@ApiController(value = "/auth")
+@RestController
+@RequestMapping(value = "/auth", produces = "application/json")
 public class AuthController {
 
     private final IUserService userService;
