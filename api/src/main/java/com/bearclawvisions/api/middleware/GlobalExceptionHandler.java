19cca716425b79622061a14ad695d8de9a123e09
bearclawvisions <113764031+bearclawvisions@users.noreply.github.com>
2026-02-22 18:19:46 +0100

    add error handling for unauthorized requests

--- api/src/main/java/com/bearclawvisions/api/middleware/GlobalExceptionHandler.java
+++ api/src/main/java/com/bearclawvisions/api/middleware/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.bearclawvisions.api.middleware;
 import com.bearclawvisions.api.contracts.ApiResponse;
 import com.bearclawvisions.api.helpers.ApiResponseBuilder;
 import com.bearclawvisions.exceptions.BusinessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,11 +15,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiResponse<String>> handleBusinessException(BusinessException e) {
-        return ApiResponseBuilder.error("Business Error", List.of(e.getMessage()));
+        return ApiResponseBuilder.error(HttpStatus.BAD_REQUEST,"Business Error", List.of(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleGenericException(Exception e) {
-        return ApiResponseBuilder.error("Internal Server Error", List.of(e.getMessage()));
+        return ApiResponseBuilder.error(HttpStatus.BAD_REQUEST, "Internal Server Error", List.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<ApiResponse<String>> handleSecurityException(SecurityException e) {
+        return ApiResponseBuilder.error(HttpStatus.UNAUTHORIZED, "Unauthorized", List.of(e.getMessage()));
     }
 }
